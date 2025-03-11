6e7900acd41de3bf7b1b8c7f079614bec6013a0f
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-03-11 10:15:57 +0530

    stacks(vertical and horizontal)

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,14 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        //align views
+        //vertical stack
         VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                //customize text
-                    .font(.title)
+            Text("Turtle Rock")
+            //customize text
+                .font(.title)
+            //horizontal stack
+            HStack {
                 Text("Joshua Tree National Park")
                     .font(.subheadline)
             }
         }
+    }
 }
 
 #Preview {
