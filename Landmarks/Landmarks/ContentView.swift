2f906ff5fa3fa05dae52f4e825d593179ad11f80
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-03-11 10:07:37 +0530

    title and subtitle

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,11 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Turtle Rock")
-        //customize text view
-            .font(.title)
-            
-    }
+            VStack {
+                Text("Turtle Rock")
+                //customize text
+                    .font(.title)
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+            }
+        }
 }
 
 #Preview {
