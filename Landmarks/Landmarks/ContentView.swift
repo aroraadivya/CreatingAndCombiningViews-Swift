30a246e1625fe309345316906455db6d5d91f9cf
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-03-11 10:16:56 +0530

    horizontal stack

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -19,6 +19,8 @@ struct ContentView: View {
             HStack {
                 Text("Joshua Tree National Park")
                     .font(.subheadline)
+                Text("California")
+                    .font(.subheadline)
             }
         }
     }
