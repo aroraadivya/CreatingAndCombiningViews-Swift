3cb66a02199af1cfdb73380b140b6ab74ea0b9ca
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-03-14 19:28:08 +0530

    Added landmark as a stored property of LandmarkRow.

--- Landmarks/Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Landmarks/Views/LandmarkRow.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    var landmark: Landmark
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
