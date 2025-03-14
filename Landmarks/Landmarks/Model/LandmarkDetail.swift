817fde0ea1028a812298f4614288bc9c691b66cf
Divya Arora <111882666+aroraadivya@users.noreply.github.com>
2025-03-14 19:58:16 +0530

    contents from content view

---
+++ Landmarks/Landmarks/Model/LandmarkDetail.swift
@@ -0,0 +1,52 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Divya Arora on 14/03/25.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+//        Text("Hello, World!")
+        //contents from content view
+        VStack {
+            MapView()
+                .frame(height: 300)
+            
+            CircleImage()
+            //to layer image view on top of map
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                //customize text
+                    .font(.title)
+                //horizontal stack
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here")
+                    
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
